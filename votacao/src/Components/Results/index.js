266a6d32568ca9c7b3e1bcaaf4529e04b8941a57
rodrigogomesrc <rocha.rodrigogomes@gmail.com>
2021-07-10 11:54:20 -0300

    results component

--- votacao/src/Components/Results/index.js
+++ votacao/src/Components/Results/index.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
 
-function Results() {
+function Results({options}) {
     return (
         <div>
-            Results
+            <p>Results</p>
+            {
+                renderVotingResults(options)
+            }
         </div>
     )
+}
+
+const renderVotingResults = (options) => {
+    let totalCount = 0;
+    options.map(option => totalCount += option.count);
+    return(
+        options.map(option => <p key={option.text}>{renderOptionResultText(totalCount, option)}</p>)
+    )
+}
+
+const renderOptionResultText = (totalCount, option) => {
+    return `${option.text} - ${option.count} votes (${calculatePercentage(option.count, totalCount)}%)`;
+}
+
+const calculatePercentage = (optionCount, totalCount) => {
+    return Math.round((optionCount / totalCount) * 100);
 }
 
 export default Results
--- votacao/src/stories/Results.stories.js
+++ votacao/src/stories/Results.stories.js
@@ -1,12 +1,11 @@
 import React from 'react' ;
 import Results from '../Components/Results';
 
-
 export default {
     title: 'Results',
     component: Results,
     argTypes: {
-       
+       options: []
     }
 };
 
@@ -16,5 +15,9 @@ const Template = (args) => <Results {...args}/>
 export const results = Template.bind();
 
 results.args = {
-  
+  options : [
+      {text: "Sim", count: 12}, 
+      {text: "Talvez", count: 8}, 
+      {text: "Não", count: 10}
+    ]
 }
