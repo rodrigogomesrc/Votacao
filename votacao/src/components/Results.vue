9b091715b2fe48bc2736735d1dd092367ff6306e
rodrigogomesrc <rocha.rodrigogomes@gmail.com>
2021-07-16 19:30:07 -0300

    results component finished

--- votacao/src/components/Results.vue
+++ votacao/src/components/Results.vue
@@ -1,12 +1,44 @@
 <template>
-    <p>this is the results component</p>    
+    <div>
+        <p v-for="option in results" :key="option">{{option}}</p>  
+    </div>  
 </template>
 
 <script>
     export default {
         name: 'results',
         props: {
-            msg: String
-        }  
+            options: []
+        }, 
+        data(){
+            return {
+                totalVotes: 0,
+                results: []
+            }
+        }, 
+        methods: {
+            loadResults(){
+                this.countVotes();
+                this.options.forEach((option, index) => {
+                    this.results.push(this.renderResult(option, index))
+                })
+            },
+            countVotes(){
+                this.options.forEach(option => {
+                    this.totalVotes += option.count;
+                    console.log(this.totalVotes);
+                });
+            },
+            calculatePercentage(optionCount){
+                return Math.round((optionCount / this.totalVotes) * 100)
+            },
+            renderResult(option, index){
+                return `${index + 1}. ${option.text} - ${option.count} votes 
+                (${this.calculatePercentage(option.count)}%)`
+            }
+
+        }, mounted(){
+            this.loadResults();
+        }
     }
 </script>
--- votacao/src/stories/Results.stories.js
+++ votacao/src/stories/Results.stories.js
@@ -3,21 +3,26 @@ import Results from '../components/Results';
 export default {
   title: 'Results',
   component: Results,
+  props: {
+    options: []
+  },
+  argTypes: {
+    options: {Control: "array"}
+  },
 };
 
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   components: { Results },
   template:
-    '<results/>',
+    '<results :options="options"/>',
 });
 
 export const results1 = Template.bind({});
 results1.args = {
-
-};
-
-export const results2 = Template.bind({});
-results2.args = {
-    
+   options: [
+        {text: "Sim", count: 3},
+        {text: "Não", count: 8},
+        {text: "Talvez", count: 12}
+    ]
 };
