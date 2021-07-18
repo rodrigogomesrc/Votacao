d2a6375111be88f6b4f65b7120922e6334c73e5d
rodrigogomesrc <rocha.rodrigogomes@gmail.com>
2021-07-18 12:14:11 -0300

    created results story

---
+++ votacao/src/Components/Results.svelte
@@ -0,0 +1,37 @@
+<script>
+    import { onMount } from 'svelte';
+
+    export let options = [];
+    let results = [];
+    let totalVotes = 0;
+
+    onMount(async () => {
+        loadResults();
+	});
+
+    const loadResults = () => {
+        countVotes();
+        options.forEach((option, index) => {
+            results = [...results, renderResult(option, index)]
+        })
+    }
+
+    const renderResult = (option, index) => {
+        return `${index + 1}. ${option.text} - ${option.count} votes (${calculatePercentage(option.count)}%)`
+    }
+
+    const calculatePercentage = (optionCount) => {
+        return Math.round((optionCount / totalVotes) * 100)
+    }
+
+    const countVotes = () => {
+        options.forEach(option => {
+            totalVotes += option.count;
+        });
+    }
+
+</script>
+
+{#each results as result}
+<p>{result}</p>
+{/each}
---
+++ votacao/src/stories/Results.stories.svelte
@@ -0,0 +1,34 @@
+<script>
+    import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+    import Results from "../Components/Results.svelte";
+
+    const options = [
+        {text: "Sim", count: 3},
+        {text: "Não", count: 8},
+        {text: "Talvez", count: 12}
+    ];
+
+</script>
+  
+<Meta
+  title="Results"
+  component={Results}
+  argTypes={{
+    options: {Control: "array"}
+  }}
+/>
+
+<Template let:args>
+    <Results {...args} />
+</Template>
+
+<Story
+  name="Results1"
+  args={{
+    options: [
+        {text: "Sim", count: 3},
+        {text: "Não", count: 8},
+        {text: "Talvez", count: 12}
+    ],
+  }}
+/>
